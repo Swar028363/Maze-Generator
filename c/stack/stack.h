c3d7d0a43c3a7f94c4f5be9fa925346b6eee9613
Swar028363 <chaudhariswar123@gmail.com>
2025-05-09 17:31:16 +0530

    changed few datatypes and fixed error with the struct

--- c/stack/stack.h
+++ c/stack/stack.h
@@ -1,9 +1,8 @@
 #ifndef STACK_H
 #define STACK_H
 
-
 #include <stdint.h>
-
+#include <stdbool.h>
 
 struct stackNode {
 	void *data; // void Pointer to the data
@@ -14,20 +13,20 @@ struct stackNode {
 struct Stack {
 	struct stackNode *top; // Pointer to the top node
 	uint32_t size;		   // Size of the stack
-	int32_t capacity;	   // Maximum capacity of the stack. -1 if no limit
+	int32_t capacity;	   // Maximum capacity of the stack. negative if no limit
 };
 
 
-Stack *createStack(int32_t capacity); // Create a stack with a given capacity
+struct Stack *createStack(int32_t capacity); // Create a stack with a given capacity
 
-void freeStack(Stack *stack, void (*freeData)(void*)); // Destroy the stack and free memory
+void freeStack(struct Stack *stack, void (*freeData)(void*)); // Destroy the stack and free memory
 
-void push(Stack *stack, void *data); // Push data onto the stack
+bool push(struct Stack *stack, void *data); // Push data onto the stack
 
-void *pop(Stack *stack); // Pop data from the stack
+void *pop(struct Stack *stack); // Pop data from the stack
 
-void *peek(Stack *stack); // Peek at the top data without removing it
+void *peek(struct Stack *stack); // Peek at the top data without removing it
 
-int32_t isEmpty(Stack *stack); // Check if the stack is empty
+bool isEmpty(struct Stack *stack); // Check if the stack is empty
 
 #endif // !STACK_H
